c5d1445ee789ab3484d046bd2e286e67c6f02076
animalki9 <143405935+animalki9@users.noreply.github.com>
2025-01-29 22:20:05 +0100

    yfr

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-// Funksjon for å oppdatere innhold når en kategori blir klikket
-function updateCategoryContent(category) {
-    const selectedResource = resources.filter((resource) => resource.category === category);
-
-    if (selectedResource.length > 0) {
-        const resource = selectedResource[0];
-        const sourcesHTML = resource.sources
-            .map(
-                (source) =>
-                    `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`
-            )
-            .join("");
-
-        document.getElementById("content").innerHTML = `
-            <article>
-                <h2>${resource.category}</h2>
-                <p>${resource.text}</p>
-                <ul>${sourcesHTML}</ul>
-            </article>
-        `;
-    } else {
-        console.error(`Category not found: ${category}`);
-    }
-}
-
 // Hent alle knappene og legg til eventlistener
 const buttons = Array.from(document.querySelectorAll(".category-btn"));
 
@@ -49,3 +24,30 @@ buttons.filter((button, index) => index === 0).map((button) => {
 });
 //buttons[0].classList.add("active");
 //updateCategoryContent(buttons[0].dataset.category);
+
+// Funksjon for å oppdatere innhold når en kategori blir klikket
+function updateCategoryContent(category) {
+    const selectedResource = resources.filter((resource) => resource.category === category);
+
+    if (selectedResource.length > 0) {
+        const resource = selectedResource[0];
+        const sourcesHTML = resource.sources
+            .map(
+                (source) =>
+                    `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`
+            )
+            .join("");
+
+        document.getElementById("content").innerHTML = `
+            <article>
+                <h2>${resource.category}</h2>
+                <p>${resource.text}</p>
+                <ul>${sourcesHTML}</ul>
+            </article>
+        `;
+    } else {
+        console.error(`Category not found: ${category}`);
+    }
+}
+
+
